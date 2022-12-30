b93eacac6be434620a89e08191fffbd977e0ca20
tuanta <tuantran.kum@gmail.com>
2022-12-30 15:02:09 +0700

    test-vercel

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          test - 2022
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          TUANTRAN - REACTJS
         </a>
       </header>
     </div>
